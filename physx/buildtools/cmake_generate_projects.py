bbf7c0de9738c99046c9d6daf57779b4decf95ef
moraaar <moraaar@amazon.com>
2022-12-03 12:29:52 +0000

    Fixed bad merge
    Signed-off-by: moraaar <moraaar@amazon.com>

--- physx/buildtools/cmake_generate_projects.py
+++ physx/buildtools/cmake_generate_projects.py
@@ -213,10 +213,10 @@ class CMakePreset:
         elif self.targetPlatform == 'mac64':
             outString = outString + ' -DTARGET_BUILD_PLATFORM=mac'
             outString = outString + ' -DPX_OUTPUT_ARCH=x86'
+            return outString
         elif self.targetPlatform == 'macArm64':
             outString = outString + ' -DTARGET_BUILD_PLATFORM=mac'
             outString = outString + ' -DPX_OUTPUT_ARCH=arm'
-            return outString
             return outString
         return ''
 
